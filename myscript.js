fc9f00a20a389bbe166a67d6feabb0ccc82bdb87
Pankajkumar90 <pankajbaliyan90@gmail.com>
2022-07-12 15:38:33 +0530

    improve UI & functioning

--- myscript.js
+++ myscript.js
@@ -1,145 +1,153 @@
-//THIS SECTION IS FOR CREATE MATRIX FOR RADIO BUTTONS
-  for (var i = 1; i <= 60; i++) {
-    if (i % 10 - 1 == 0) {
-      let x = document.createElement("br");
-      document.getElementById("radiobtns").appendChild(x);
-    }
-    let x = document.createElement("INPUT");
-    x.setAttribute("type", "radio");
-    x.setAttribute("class", "radiogrp");
-    x.setAttribute("name", "radiogroup");
-    x.setAttribute("disabled", "");
-    x.setAttribute("id", `${i}`);
-    document.getElementById("radiobtns").appendChild(x);
-  }
-
-//FUNCTION FOR ADDING AND REMOVING CLASS WHEN RADIO BUTTOS AUTO CLICK
-let compId;
-function hello(x){
-  x.classList.add('userselectedradio')
-  compId=x.id
-  setTimeout(()=>{
-    x.classList.remove('userselectedradio')
-  },1000)
-}
-//THIS SECTION WILL CLICKS RANDOMLY (AS A SYSTEM)
-let systemClicks = [];
-function autoclick() {
-  let array = document.getElementsByName("radiogroup");
-  let randomNumber = Math.floor(Math.random() * 60);
-  array[randomNumber].addEventListener('click',hello(array[randomNumber]))
-}
-//THIS SECTION WILL CLICKS (AS A USER) & UPDATE SCORE & CALCULATE PERCENTAGE
-let userClicks = [];
+// DEFINE & DECLARE VERIABLES
+let inputTime = document.getElementById("inputTime");
+let radiobtns = document.getElementById("radiobtns");
+let endGameBtn = document.getElementById("stopButton");
+let inputScore = document.getElementById("inputScore");
+let startGameBtn = document.getElementById("startButton");
+let allRadios = document.getElementsByClassName("radiogrp");
+let resultMessage = document.getElementById("resultMessage");
+let allRadioButtons = document.getElementsByName("radiogroup");
+let valueAccordingTime = document.getElementById("valueAccordingTime");
+let i;
 let userId;
 let score = 0;
+let intervalId;
+let inputTimeValue;
 let Percentage = 0;
-document.addEventListener("DOMContentLoaded",function () {
-  const allCards = document.getElementsByClassName('radiogrp');
-    Array.prototype.forEach.call(allCards, function (element) {
-        element.addEventListener('click', function () {
-          userId=element.id
-          if(compId==userId){
-            score = score + 1;
-            document.getElementById("score").value = score;
-          }else{
-            score=score-1
-            document.getElementById("score").value = score;
-          }
-          Percentage = (score / totalTime) * 100;
-        },{once:true});
-    });
-})
+let setAutoClickTime;
+let inputTimeValueStore;
+let systemClickedRadioID;
 
-//SECTION FOR TIMING CONTROL
-function startTimer() {
-  let obj = document.getElementById("timer");
-  let seconds;
-  seconds = Number(obj.display.value);
-  if (seconds <= 0) {
-    clearInterval(Id);
-    clearInterval(tymint);
-    seconds = 0;
-    document.getElementById("restrtbtn").disabled = false;
-    document.getElementById("stopbtn").disabled = true;
-    mypercentage();
-    let x = document.getElementsByClassName("radiogrp");
-    let i;
-    for (i = 0; i < x.length; i++) {
-      x[i].disabled = true;
-      x[i].checked = false;
-    }
-  } else {
-    seconds--;
+//FUNCTION FOR UPDATE TIME IN TOP PARAGRAPH ACCOURDING TO USER INPUT TIME/DEFAULT TIME ON FIRST WINDOW LOAD
+function updateTime() {
+  inputScore.value = 0;
+  valueAccordingTime.innerHTML = Number(inputTime.value);
+}
+
+//THIS SECTION IS FOR CREATE MATRIX FOR RADIO BUTTONS
+for (i = 1; i <= 60; i++) {
+  if ((i % 10) - 1 == 0) {
+    let lineBreak = document.createElement("br");
+    radiobtns.appendChild(lineBreak);
   }
-  obj.display.value = seconds;
+  let inputRadio = document.createElement("INPUT");
+  inputRadio.setAttribute("type", "radio");
+  inputRadio.setAttribute("class", "radiogrp");
+  inputRadio.setAttribute("name", "radiogroup");
+  inputRadio.setAttribute("disabled", "");
+  inputRadio.setAttribute("id", `${i}`);
+  radiobtns.appendChild(inputRadio);
 }
-
-//STOP BUTTON
-function StopandTimer() {
-  document.getElementById("restrtbtn").disabled = false;
-  clearInterval(tymint);
-  clearInterval(Id);
-  const data = document.querySelectorAll(".radiogrp")
-  data.forEach(function (card) {
-    card.disabled = true;
-  })
-}
-
-//RESTART BUTTON
-function restartgame() {
-  document.getElementById("startbtn").disabled = true;
-  document.getElementById("restrtbtn").disabled = true;
-  document.getElementById("stopbtn").disabled = false;
-  document.getElementById("Time").value = 30;
-  document.getElementById("timeaccvalue").innerHTML = 30;
-  score = 0;
-  document.getElementById("score").value = `${score}`;
-  data = document.querySelectorAll(".radiogrp")
-  data.forEach(function (card) {
-    card.disabled = false;
-  })
-  Id = setInterval("startTimer()", 1000);
-  tymint = setInterval(autoclick, 1000);
-}
-
 
 //WHEN YOU CLICK ON START BUTTON THIS SECTION WORKS.
-function StartandTimer() {
-  document.getElementById("startbtn").disabled = true;
-  document.getElementById("restrtbtn").disabled = true;
-  document.getElementById("stopbtn").disabled = false;
-  document.getElementsByName("radiogroup").disabled = false;
-  Id = setInterval("startTimer()", 1000);
-  tymint = setInterval(autoclick, 1000);
-  document.getElementById("score").value = `${score}`;
-  if (score == 0) {
-    document.getElementById("result").innerHTML = "Result Will be Shown here.";
-  }
-  let x = document.getElementsByClassName("radiogrp");
-  let i;
-  for (i = 0; i < x.length; i++) {
-    x[i].disabled = false;
+function StartGame() {
+  inputScore.value = 0;
+  inputTime.readOnly = true;
+  endGameBtn.disabled = false;
+  startGameBtn.disabled = true;
+  inputTimeValueStore = inputTime.value;
+  inputTimeValue = Number(inputTime.value);
+  setAutoClickTime = setInterval(autoclick, 1000);
+  valueAccordingTime.innerHTML = Number(inputTime.value);
+
+  intervalId = window.setInterval(function () {
+    reduceTime();
+  }, 1000);
+
+  for (i = 0; i < allRadios.length; i++) {
+    allRadios[i].disabled = false;
   }
 }
-//GIVE FEEDBACK ACCORDING PERCENTAGE
-let perr = 0;
-function mypercentage() {
-  perr = Percentage;
-  if (perr >= 80) {
-    document.getElementById("result").innerHTML = "Congratulations";
-  } else if (perr >= 60 && perr < 80) {
-    document.getElementById("result").innerHTML =
-      "Good job & try again to get more scores";
+
+//FUNCTION TO REDUCE TIME IN INPUT FIELD AFTER CLICK ON START GAME BTN
+function reduceTime() {
+  if (inputTimeValue <= 0) {
+    clearInterval(intervalId);
+    clearInterval(setAutoClickTime);
+    inputTime.readOnly = false;
+    for (i = 0; i < allRadios.length; i++) {
+      allRadios[i].disabled = true;
+      allRadios[i].checked = false;
+    }
+    Percentage = (score / inputTimeValueStore) * 100;
+    mypercentage();
   } else {
-    document.getElementById("result").innerHTML =
-      "Sorry, better luck next time";
+    inputTimeValue--;
+    inputTime.value = inputTimeValue;
   }
 }
-//CHANGE THE TIME IN PARAGRAPH ACCORDING TO TIME CHOOSEN
-let totalTime;
-function timemanage() {
-  totalTime = document.getElementById("Time").value;
-  let timesync = document.getElementById("Time").value;
-  document.getElementById("timeaccvalue").innerHTML = timesync;
+
+//WHEN YOU CLICK ON END GAME BUTTON THIS SECTION WORKS.
+function EndGame() {
+  inputScore.value = 0;
+  startGameBtn.disabled = false;
+  endGameBtn.disabled = true;
+  clearInterval(intervalId);
+  clearInterval(setAutoClickTime);
+  inputTime.value = 30;
+  inputTime.readOnly = false;
+
+  for (i = 0; i < allRadios.length; i++) {
+    allRadios[i].disabled = true;
+  }
+  Percentage();
+}
+
+//THIS SECTION WILL CLICKS RANDOMLY (AS A SYSTEM)
+function autoclick() {
+  let randomNumber = Math.floor(Math.random() * 60);
+  allRadioButtons[randomNumber].addEventListener(
+    "click",
+    systemClickedRadio(allRadioButtons[randomNumber])
+  );
+}
+
+//FUNCTION FOR ADDING AND REMOVING CLASS WHEN RADIO BUTTOS AUTO CLICK
+function systemClickedRadio(radioForClick) {
+  radioForClick.classList.add("systemClickRadio");
+  systemClickedRadioID = radioForClick.id;
+  setTimeout(() => {
+    radioForClick.classList.remove("systemClickRadio");
+  }, 1000);
+}
+
+// THIS SECTION WILL CLICKS (AS A USER) & UPDATE SCORE & CALCULATE PERCENTAGE
+document.addEventListener("DOMContentLoaded", function () {
+  Array.prototype.forEach.call(allRadios, function (element) {
+    element.addEventListener(
+      "click",
+      function () {
+        userId = element.id;
+        if (systemClickedRadioID == userId) {
+          score = score + 1;
+          if (score <= 0) {
+            score = 0;
+            inputScore.value = score;
+          } else {
+            inputScore.value = score;
+          }
+        } else {
+          score = score - 1;
+          if (score <= 0) {
+            score = 0;
+            inputScore.value = score;
+          } else {
+            inputScore.value = score;
+          }
+        }
+      },
+      { once: true }
+    );
+  });
+});
+
+//GIVE FEEDBACK ACCORDING PERCENTAGE
+function mypercentage() {
+  if (Percentage >= 80) {
+    resultMessage.innerHTML = "Congratulations";
+  } else if (Percentage >= 60 && Percentage < 80) {
+    resultMessage.innerHTML = "Good job & try again to get more scores";
+  } else {
+    resultMessage.innerHTML = "Sorry, better luck next time";
+  }
 }
